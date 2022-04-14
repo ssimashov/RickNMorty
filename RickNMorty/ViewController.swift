79dc36220e6380b7d8030059b7065db71e2f2c3e
Sergey Simashov <simashov@gmail.com>
2022-04-14 22:07:54 +0300

    Initial Commit

---
+++ RickNMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickNMorty
+//
+//  Created by Sergey Simashov on 14.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
